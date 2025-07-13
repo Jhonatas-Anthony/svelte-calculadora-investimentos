52a2cd8a6661925a69f7f3e350673545aedcf9dd
Jhonatas-Anthony <jhonatasanthony6@gmail.com>
2025-07-13 01:46:14 -0300

    feat: novas mudanças para build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+// Substitua pelo NOME do seu repositório GitHub:
+const repo = 'svelte-calculadora-investimentos'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repo}/`,
   plugins: [svelte()],
 })
